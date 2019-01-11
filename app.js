2cbc4ed32452ac104cd560b35da12a46a9de2afc
Nartay <mastaok02@gmail.com>
2019-01-11 17:24:54 +0600

    Refactored and added localstorage

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cheerio = require('cheerio');
+const request = require('request');
+const requestPromise = require('request-promise');
+
 const app = express();
+
 app.set('view engine', 'ejs'); // Шаблонизатор
 app.use(bodyParser.urlencoded({ extended: true })); //для получение post запроса
 app.use(express.static(path.join(__dirname, 'public')));
@@ -10,71 +15,69 @@ app.use(
   express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
 );
 
-var isEntered = false;
-var interval;
-var user = '';
-var pass = '';
-var sum = [];
-var name = '';
-var cheerio = require('cheerio');
-var request = require('request');
-var jar = request.jar();
-var url = 'http://www.alivemax.com/';
-var asdF = function () {
-  request(
-    {
-      uri: url + 'login',
-      method: 'POST',
-      form: {
-        LOGIN: '1',
-        USER: user,
-        PASS: pass
-      },
-      jar: jar,
-      followRedirect: true,
-      followAllRedirects: true
-    },
-    function (error, response, body) {
-      var ch = cheerio.load(body);
-      name = ch('.nameBanner').text();
-      if (!error) {
-        request(
-          {
-            uri: url + 'earnings',
-            method: 'GET',
-            jar: jar
-          },
-          function (err, resp, html) {
-            if (!err) {
-              var $ = cheerio.load(html);
-              var rows = $('.mout');
-              for (var i = 0; i < rows.length; i++) {
-                var row = rows.eq(i).children();
-                for (var j = 0; j < 1; j++) {
-                  var date = row.eq(1).text();
-                  var money = row.eq(3).text();
-                  var arrTwo = [[], []];
-                  arrTwo[0].push(date);
-                  arrTwo[1].push(money);
-                  sum.push(arrTwo);
-                }
-              }
-              isEntered = true;
-              return isEntered;
-            } else {
-              console.log(err);
-            }
-          }
-        );
-      } else {
-        console.log('There is an ERROR !! ' + error);
-      }
+let user = 'nartay777';
+let pass = 'dimaw2007';
+let sum = [];
+let userName = '';
+let jar = request.jar();
+let url = 'http://www.alivemax.com/';
+let parseSalaries = function (rows) {
+  let sum = [];
+  for (let i = 0; i < rows.length; i++) {
+    let row = rows.eq(i).children();
+    for (let j = 0; j < 1; j++) {
+      let date = row.eq(1).text();
+      let money = row.eq(3).text();
+      let arrTwo = [[], []];
+      arrTwo[0].push(date);
+      arrTwo[1].push(money);
+      sum.push(arrTwo);
     }
-  );
-  return true;
+  }
+  return sum;
 };
+
+
+
+const login = function () {
+  const loginOptions = {
+    uri: url + 'login',
+    method: 'POST',
+    form: {
+      LOGIN: '1',
+      USER: user,
+      PASS: pass
+    },
+    jar: jar,
+    followRedirect: true,
+    followAllRedirects: true
+  };
+
+  return requestPromise(loginOptions);
+};
+const getSalariesPage = function () {
+  const options = {
+    uri: url + 'earnings',
+    method: 'GET',
+    jar: jar
+  }
+  return requestPromise(options);
+};
+
+const getSalaries = function (html) {
+  const $ = cheerio.load(html);
+  const rows = $('.mout');
+  const salaries = parseSalaries(rows);
+  return salaries;
+};
+
+const getName = function (body) {
+  var page = cheerio.load(body);
+  userName = page('.nameBanner').text();
+  return userName;
+}
+
 app.get('/', function (req, res) {
-  isEntered = false;
   sum = [];
   user = '';
   pass = '';
@@ -83,15 +86,19 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   user = req.body.username;
   pass = req.body.password;
-  asdF();
-  interval = setInterval(function () {
-    if (isEntered === true) {
-      clearInterval(interval);
-      res.redirect('/create');
-    }
-  }, 1000);
+  var loginPage = login();
+
+  let dataUserName = loginPage.then(getName);
+  let dataSum = loginPage.then(getSalariesPage).then(getSalaries);
+
+  Promise.all([dataUserName, dataSum]).then((data) => {
+    userName = data[0];
+    sum = data[1];
+    res.redirect('/create');
+  }).catch(err => console.log(err));
+
 });
 app.get('/create', function (req, res) {
-  res.render('create', { sum: sum, name: name });
+  res.render('create', { sum: sum, name: userName });
 });
 module.exports = app;
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('./config');
 database()
   .then(info => {
     console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
-    app.listen(config.PORT, function() {
+    app.listen(config.PORT, function () {
       console.log(`Example app listening on port ${config.PORT}`);
     });
   })
