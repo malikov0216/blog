0f2c5477f39057f9daa5bd233c7ca4b1bc0efcbf
Nartay <mastaok02@gmail.com>
2019-01-13 03:48:13 +0600

    bla bla

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
 const app = require('./app');
-const database = require('./database');
 const config = require('./config');
-
-database()
-  .then(info => {
-    console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
-    app.listen(config.PORT, function () {
-      console.log(`Example app listening on port ${config.PORT}`);
-    });
-  })
-  .catch(() => {
-    console.error('Unable to connect database');
-    process.exit(1);
-  });
